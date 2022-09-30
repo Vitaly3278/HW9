3fccc3b2253ddc8886bfe1911a0babbb51c546ec
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-30 19:08:34 +0300

    Numbers N to 1

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -2,17 +2,17 @@
 // Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
 // Выполнить с помощью рекурсии.
 
-int Number (int n)
+int Number(int n)
 {
     if (n == 0)
     {
         return 0;
     }
     Console.Write($" {n}");
-    return Number (n-1);
+    return Number(n - 1);
 }
 
 Console.Write("Введите N: ");
-int n = int.Parse (Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
 Console.WriteLine();
-Number (n);
+Number(n);
