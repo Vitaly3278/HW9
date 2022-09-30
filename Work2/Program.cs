9b4c828e012f50c64fb26a985dd5d1ba1093bdb1
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-30 19:20:32 +0300

    Sum N to M

---
+++ Work2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNumber(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равна: {sum}");
+        return n;
+    }
+    Console.Write($" {m}");
+    sum = sum + (m++);
+    return SumNumber(m, n, sum);
+}
+
+Console.Write("Введите m и n: ");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+SumNumber(m, n, 0);
